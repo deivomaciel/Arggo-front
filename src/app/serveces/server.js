3c42ee5907882df338b2c08973570ace2edd5f77
(Deivide <(deividemaciel2@gmail.com)>
2023-11-03 22:48:28 -0300

    refact: removed user id log from server.js

--- src/app/serveces/server.js
+++ src/app/serveces/server.js
@@ -1,5 +1,5 @@
 import socketIO from 'socket.io-client'
-const url = 'http://localhost:3000'
+const url = process.env.NEXT_PUBLIC_SERVER_URL
 export const ioClient = socketIO.connect(url)
-export const connect = () => ioClient.on('connect', () => console.log('connected!', ioClient.id))
+export const connect = () => ioClient.on('connect', () => console.log('connected!'))
 
